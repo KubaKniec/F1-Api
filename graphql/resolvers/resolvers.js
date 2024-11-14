687912ef049a034227df395eb8e60988a8dc98dc
KubaKniec <jakub.kniec02@gmail.com>
2024-11-14 22:16:44 +0100

    graphQL added

--- graphql/resolvers/resolvers.js
+++ graphql/resolvers/resolvers.js
@@ -1,10 +1,16 @@
 import circuit from "../JSONsForGraphQl/Circuit.json" assert { type: "json" };
 import race from "../JSONsForGraphQl/race.json" assert { type: "json" };
+import driver from "../JSONsForGraphQl/driver.json" assert { type: "json" };
+import constructor from "../JSONsForGraphQl/Constructor.json"  assert { type: "json" };
+
 
 export const resolvers = {
     Query: {
         circuits: () => circuit.Circuit,
-        race: () => race.Race
+        race: () => race.Race,
+        driver: () => driver.Driver,
+        constructor: () => constructor.Constructor
+
     },
     // Circuit dodac obiekt
     //i dostac potem konkretny obiekt
